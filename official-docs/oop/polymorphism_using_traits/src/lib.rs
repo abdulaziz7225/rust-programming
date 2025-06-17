68450a39525d76e0608dfe65b1e98bcef3495dd4
Abdulaziz Musaev <amusaev7225@gmail.com>
2025-06-17 02:11:11 +0200

    Learn and implement object-oriented pattern from official doc

---
+++ official-docs/oop/polymorphism_using_traits/src/lib.rs
@@ -0,0 +1,44 @@
+pub trait Draw {
+    fn draw(&self);
+}
+
+// Trait objects allow for multiple concrete types to fill in for the trait object at runtime
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        todo!;
+    }
+}
+
+// A generic type parameter with trait bounds
+// pub struct Screen<T: Draw> {
+//     pub components: Vec<T>,
+// }
+
+// impl<T> Screen<T>
+// where
+//     T: Draw,
+// {
+//     pub fn run(&self) {
+//         for component in self.components.iter() {
+//             component.draw();
+//         }
+//     }
+// }
